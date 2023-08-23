35b5ed047c4cd38056aa0b99cdc5a6d12dd569ac
Carlos Marin <33185069+carlosmarin88@users.noreply.github.com>
2023-08-22 21:17:05 -0300

    add modify FirstApp

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,28 @@
+//import {Fragment} from 'react';
+
 // 'rafc' shortcut for create component
-export const FirstApp = () => {
+
+const newMessage = {
+    message: 'Hello World',
+    title: 'Charly'
+};
+
+const greeting = (message) => `Hi ${message.title}, I have a  message for you "${message.message}"`;
+
+
+export const FirstApp = ({title = 'Hola, soy Charly', subTitle}) => {
+    
+    //console.log(props);
+
+    // <></> = <Fragment></Fragment>
     return(
-        <h1>Charly</h1>
+        <> 
+            <h1>{title}</h1>
+            {/*<h1>{greeting(newMessage)}</h1>*/}
+            {/*<code>{JSON.stringify(newMessage)}</code>*/}
+            <p>{subTitle}</p>
+        </>
+        
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 import { HelloWorldApp } from './HelloWorldApp';
 
+import './styles.css';
+
+// yarn dev for run project
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
     <React.StrictMode>
-        <HelloWorldApp/>
-        <FirstApp/>
+        {/*<HelloWorldApp/>*/}
+        <FirstApp title="Hola, soy Goku"/>
     </React.StrictMode>
 );
